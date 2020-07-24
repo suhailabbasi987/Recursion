4254e56afc9c498285b453f85613f397b1ba3dad
suhailabbasi987 <51741273+suhailabbasi987@users.noreply.github.com>
2020-07-24 23:15:11 +0530

    Update codass.java

--- codass.java
+++ codass.java
@@ -1,1 +1,19 @@
-hello
+public class codass{
+public static void main( String []  args)
+{
+  PrintIncreasingNumber(5);
+    
+}
+  public class void printIncreasingNumber(int n)
+  {
+    if(n==0)
+    {
+      return;
+    }
+    
+    printIncreasingNumber(n-1);
+    System.out.println(n);
+    
+  }
+  
+}
