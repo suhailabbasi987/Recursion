b2c3b4403150433f064b2b768bf9cdb3e08dfd53
suhailabbasi987 <51741273+suhailabbasi987@users.noreply.github.com>
2020-08-15 02:41:20 +0530

    Update codass.java

--- codass.java
+++ codass.java
@@ -1,10 +1,14 @@
 public class codass{
 public static void main( String []  args)
 {
+  int [] arr = {1,2,3,4,5};
   pintIncreasingNumber(5);
   printDecreasingNumber(5);
   printIncreasingDecreasing(5);
   towerOfHanoi("A","B","C",3);
+  printArray(arr,0);
+  printReverseArray(arr,0);
+  printIncDecArray(arr,0);
   
     
 }
@@ -85,5 +89,41 @@ public static void towerOfHanoi(String src, String helper,String dest,int n)
   towerOfHanoi(helper,src,dest,n-1);
 }
 
+
+public static void printArray(int [] arr,int vidx)
+{
+  if(vidx==arr.length)
+  {
+    return;
+  }
   
+  System.out.println(arr[vidx]);  // this line print before call so its print value as it is as array order.
+  printArray(arr,vidx+1);
+  
+}
+  
+public static void printReverseArray(int [] arr,int vidx)
+{
+
+  if(vidx == arr.length)
+  {
+    return;
+  }
+  printReverseArray(arr,vidx+1);  
+  System.out.println(arr[vidx]);  // this line print after calls so after reaching basecase on the way of return n drop back this line will be executed
+
+}
+
+public static void printIncDecArray(int [] arr,int vidx)
+{
+  if(arr.length == vidx)
+  {
+    return ;
+  }
+   
+  System.out.println(arr[vidx]);  // this line executed before calls 
+  printIncDecArray(arr,vidx+1);   // this is call  
+  System.out.println(arr[vidx]);  // this line executed after return in function
+}
+
 }
